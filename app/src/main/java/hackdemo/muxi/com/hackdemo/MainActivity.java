a65fb5bf0226a2a594e8ad157a978aac5177fae8
kildares <kildare.silveira@gmail.com>
2018-03-08 17:33:28 -0300

    ADD reports

--- app/src/main/java/hackdemo/muxi/com/hackdemo/MainActivity.java
+++ app/src/main/java/hackdemo/muxi/com/hackdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package hackdemo.muxi.com.hackdemo;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mValor;
     private Button mPagar;
     private PWPServicesManager pwpServicesManager;
+
+    private String mLastProcessedVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +60,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(MainActivity.this,ReportActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onClick(View view) {
         if(view.getId() == mPagar.getId()){
             makePayment();
@@ -83,6 +94,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             pwpsTransaction.setCurrency(PWPSTransaction.CurrencyType.BRL);
             pwpsTransaction.setType(PWPSTransaction.TransactionType.CREDIT);
             pwpServicesManager.makePayment(pwpsTransaction,MainActivity.this);
+
+            mLastProcessedVal = valor;
         }
 
     }
@@ -99,9 +112,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void saveTransaction()
     {
-        //SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        //sharedPreferences.edit()
+        String keyTotals = getString(R.string.key_approved_totals);
+        String keyNumber = getString(R.string.key_approved_number);
+
+        String totals = sharedPreferences.getString(keyTotals,"0");
+        String numbers = sharedPreferences.getString(keyNumber,"0");
+
+        Long totalVal = Long.parseLong(totals);
+        Integer numberTrx = Integer.parseInt(numbers);
+
+        totalVal += Long.parseLong(mLastProcessedVal);
+        numberTrx++;
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(keyTotals, totalVal.toString());
+        editor.putString(keyNumber, numberTrx.toString());
+
+        editor.apply();
     }
 
     @Override
--- app/src/main/java/hackdemo/muxi/com/hackdemo/ReportActivity.java
+++ app/src/main/java/hackdemo/muxi/com/hackdemo/ReportActivity.java
@@ -1,13 +1,44 @@
 package hackdemo.muxi.com.hackdemo;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReportActivity extends AppCompatActivity {
+
+    TextView mQuantity;
+    TextView mTotals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+
+        mQuantity = findViewById(R.id.tv_num_trx);
+        mTotals = findViewById(R.id.tv_totals);
+
+        loadTotals();
+
+    }
+
+
+    public void loadTotals()
+    {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String totalKey = getString(R.string.key_approved_totals);
+        String numberKey = getString(R.string.key_approved_number);
+
+        String totals = sharedPreferences.getString(totalKey,"0");
+        String number = sharedPreferences.getString(numberKey,"0");
+
+        totals =    (totals.length() == 1) ? "0.0" + totals :
+                    (totals.length() == 2) ? "0." + totals  :
+                    totals.substring(0,totals.length()-2) + "." + totals.substring(totals.length() - 2);
+
+        mQuantity.setText("Number of approved transactions: " + number);
+        mTotals.setText("Approved Transactions totals: " + totals);
     }
 }
