48625f6baf9ce70f08f421c68f9f87436cba9147
kildares <kildare.silveira@gmail.com>
2018-03-08 14:20:22 -0300

    first commit

---
+++ app/src/main/java/hackdemo/muxi/com/hackdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package hackdemo.muxi.com.hackdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
